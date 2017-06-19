9df90a2f6ed90884a8d1af316df09f43594cc3b4
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-06-19 12:09:40 -0300

    Renaming example.

--- demo/simple_crawler.py
+++ demo/simple_crawler.py
@@ -10,7 +10,12 @@ def on_done(con, response):
     print response.fd.read()
 
 def create_connection(addr):
-    con = get(addr, '/', ssl=True)
+    headers = {
+    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', 
+    'accept-charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.7'}
+
+    con = get(addr, '/', ssl=True, headers=headers)
     xmap(con, '200', on_done)
     return con
 
@@ -18,11 +23,12 @@ if __name__ == '__main__':
     redirect = lambda con, response: \
     create_connection(response.headers['location'])
 
-    con = create_connection('https://www.bol.com.br')
+    con = create_connection(sys.argv[1])
     xmap(con, '302', redirect)
     xmap(con, '301', redirect)
 
     core.gear.mainloop()
+
 
 
 
